6266e4251182f8d79c1c42404d674dbec3777187
wences-sanchez <wenceslaosanchezpino@gmail.com>
2021-07-08 18:15:47 +0200

    Add unit test
    Created unit test by appending an extra field to the input line.
    The rest of the line is completely valid.

--- src/test/java/com/iteratrlearning/shu_book/chapter_02/BankTransactionCSVParserTest.java
+++ src/test/java/com/iteratrlearning/shu_book/chapter_02/BankTransactionCSVParserTest.java
@@ -6,20 +6,39 @@ import org.junit.Test;
 import java.time.LocalDate;
 import java.time.Month;
 
+import static org.junit.Assert.*;
+
 public class BankTransactionCSVParserTest {
 
     private BankStatementParser statementParser = new BankStatementCSVParser();
 
     @Test
-    public void shouldParseOneCorrectLine() throws Exception {
+    public void shouldParseOneCorrectLine() {
         String line = "30-01-2017,-50,Tesco";
 
         BankTransaction result = statementParser.parseFrom(line);
 
-        BankTransaction expected = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
-        Assert.assertEquals(expected.getDate(), result.getDate());
-        Assert.assertEquals(expected.getAmount(), result.getAmount(), 0.0d);
-        Assert.assertEquals(expected.getDescription(), result.getDescription());
+        BankTransaction expected = new BankTransaction(
+                LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
+        assertEquals(expected.getDate(), result.getDate());
+        assertEquals(expected.getAmount(), result.getAmount(), 0.0d);
+        assertEquals(expected.getDescription(), result.getDescription());
+    }
+
+    @Test
+    public void shouldParseLongerLineWithExtraData() {
+        // Given
+        String lineWithExtraData = "07-07-2021,100,Gift,Extra field";
+
+        // When
+        BankTransaction result = statementParser.parseFrom(lineWithExtraData);
+
+        // Then
+        BankTransaction expected = new BankTransaction(
+                LocalDate.of(2021, Month.JULY, 7), 100, "Gift");
+        assertEquals(expected.getDate(), result.getDate());
+        assertEquals(expected.getAmount(), result.getAmount(), 0.0d);
+        assertEquals(expected.getDescription(), result.getDescription());
     }
 
 }
