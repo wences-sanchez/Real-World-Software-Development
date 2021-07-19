cd6c61ab89b21cedf965533c21d86aec1dde7a44
wences-sanchez <wenceslaosanchezpino@gmail.com>
2021-07-19 11:31:20 +0200

    Create static import to org.junit.Assert

--- src/test/java/com/iteratrlearning/shu_book/chapter_02/BankTransactionCSVParserTest.java
+++ src/test/java/com/iteratrlearning/shu_book/chapter_02/BankTransactionCSVParserTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import java.time.LocalDate;
 import java.time.Month;
 
+import static org.junit.Assert.*;
+
 public class BankTransactionCSVParserTest {
 
     private BankStatementParser statementParser = new BankStatementCSVParser();
@@ -20,6 +22,38 @@ public class BankTransactionCSVParserTest {
         Assert.assertEquals(expected.getDate(), result.getDate());
         Assert.assertEquals(expected.getAmount(), result.getAmount(), 0.0d);
         Assert.assertEquals(expected.getDescription(), result.getDescription());
+    }
+
+    @Test
+    public void shouldParseLongerLineWithExtraData() {
+        // Given
+        String lineWithExtraData = "07-07-2021,100,Gift,Extra field";
+
+        // When
+        BankTransaction result = statementParser.parseFrom(lineWithExtraData);
+
+        // Then
+        BankTransaction expected = new BankTransaction(
+                LocalDate.of(2021, Month.JULY, 7), 100, "Gift");
+        assertEquals(expected.getDate(), result.getDate());
+        assertEquals(expected.getAmount(), result.getAmount(), 0.0d);
+        assertEquals(expected.getDescription(), result.getDescription());
+    }
+
+    @Test
+    public void shouldHandleInvalidData() {
+        // Given
+        String invalidDataLine = "1-01-2000,-10.5,Fuel"; // Date format is: day-month-year
+
+        // When
+        BankTransaction result = statementParser.parseFrom(invalidDataLine);
+
+        // Then
+        BankTransaction expected = new BankTransaction(
+                LocalDate.of(2000, Month.JANUARY, 1), -10.5, "Fuel");
+        assertEquals(expected.getDate(), result.getDate());
+        assertEquals(expected.getAmount(), result.getAmount(), 0.0d);
+        assertEquals(expected.getDescription(), result.getDescription());
     }
 
 }
